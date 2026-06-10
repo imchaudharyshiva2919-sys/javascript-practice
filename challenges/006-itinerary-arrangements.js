763f877fbbc0ffc5af11a8042e72751745cc690f
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-06-10 23:45:49 +0530

    freeCodeCamp daily challenge #304 solution

--- challenges/006-itinerary-arrangements.js
+++ challenges/006-itinerary-arrangements.js
@@ -1,4 +1,5 @@
-/*Given a list of optional trip stops, count the number of valid itinerary arrangements.
+/* Problem: Itinerary Arrangements
+Given a list of optional trip stops, count the number of valid itinerary arrangements.
 
 The itinerary always includes:
 - breakfast
@@ -13,3 +14,25 @@ Rules:
 
 Return the total number of valid arrangements. */
 
+
+function getItineraryCount(stops) {
+    const n = stops.length;
+
+    let factorial = 1;
+    for (let i = 2; i <= n; i++) {
+        factorial *= i;
+    }
+
+    return factorial * (2 * n - 3);
+}
+
+/*
+Concepts Used:
+- Arrays
+- Permutations
+- Combinatorics
+- Counting Arrangements
+- Constraints Handling
+- Mathematical Reasoning
+- Functions
+*/
