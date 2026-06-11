290a5669175d75cc22396cc0f123e86213ca5f35
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-06-11 23:38:21 +0530

    Idea-ranking , daily challenge #305 solution , freeCodeCamp

--- challenges/007-idea-rankings.js
+++ challenges/007-idea-rankings.js
@@ -6,3 +6,24 @@ expected = ((optimistic + 4 × realistic + pessimistic) / 6) × ideaNameLength
 Return the idea names sorted by expected completion time, shortest first.
 */
 
+
+function analyzeIdeas(ideas) {
+    return ideas
+        .sort((a, b) => {
+            const expectedA = ((a[1] + 4 * a[2] + a[3]) / 6) * a[0].length;
+            const expectedB = ((b[1] + 4 * b[2] + b[3]) / 6) * b[0].length;
+            return expectedA - expectedB;
+        })
+        .map(idea => idea[0]);
+}
+
+/*
+Concepts Used:
+- 2D Arrays
+- Array Sorting
+- Custom Compare Functions
+- Mathematical Calculations
+- String Length
+- Array Mapping
+- Functions
+*/
