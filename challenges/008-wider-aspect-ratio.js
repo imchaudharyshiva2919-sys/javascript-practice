1a17e5d60e84fa08cc5d91863d9892af0fc63794
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-06-12 22:38:07 +0530

    freeCodeCamp Daily challenge #292 solution

--- challenges/008-wider-aspect-ratio.js
+++ challenges/008-wider-aspect-ratio.js
@@ -24,3 +24,31 @@ Source: FreeCodeCamp
 
 
 
+function getWiderAspectRatio(dim1, dim2) {
+    const [w1, h1] = dim1.split('x').map(Number);
+    const [w2, h2] = dim2.split('x').map(Number);
+
+    const ratio1 = w1 / h1;
+    const ratio2 = w2 / h2;
+
+    let w, h;
+
+    if (ratio1 >= ratio2) {
+        w = w1;
+        h = h1;
+    } else {
+        w = w2;
+        h = h2;
+    }
+
+    function gcd(a, b) {
+        while (b !== 0) {
+            [a, b] = [b, a % b];
+        }
+        return a;
+    }
+
+    const g = gcd(w, h);
+
+    return `${w / g}:${h / g}`;
+}
