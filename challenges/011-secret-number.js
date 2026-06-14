fe288b3a5f8209a634749d7d8d489fd23b6beb97
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-06-14 22:40:23 +0530

    Add challenge 011 - Secret Number

---
+++ challenges/011-secret-number.js
@@ -0,0 +1,22 @@
+/*
+Problem: Secret Number
+
+Given a secret number and a guess, determine whether the guess is correct.
+
+Return:
+- "higher" if the secret number is higher than the guess.
+- "lower" if the secret number is lower than the guess.
+- "you got it!" if the guess is correct.
+
+Example:
+Input: (50, 30)
+Output: "higher"
+
+Concepts:
+- Functions
+- Conditional Statements
+- Comparison Operators
+- Problem Solving
+
+Source: FreeCodeCamp
+*/
