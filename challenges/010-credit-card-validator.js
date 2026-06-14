a915a628f968f9b7f85345993a7a6648e52d6eaf
Shivam Chaudhary <im.chaudharyshiva2919@gmail.com>
2026-06-14 22:18:04 +0530

    Solve challenge 007 - Credit Card Validator

--- challenges/010-credit-card-validator.js
+++ challenges/010-credit-card-validator.js
@@ -24,3 +24,26 @@ Concepts:
 
 Source: FreeCodeCamp
 */
+
+
+function isValidCard(cardNumber) {
+  let sum = 0;
+  let shouldDouble = false;
+
+  for (let i = cardNumber.length - 1; i >= 0; i--) {
+    let digit = Number(cardNumber[i]);
+
+    if (shouldDouble) {
+      digit *= 2;
+
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+
+    sum += digit;
+    shouldDouble = !shouldDouble;
+  }
+
+  return sum % 10 === 0;
+}
